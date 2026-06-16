43f961d5e21c36fcac5f51c574409f82f710fcf9
Joseph N <JosephTnied@gmail.com>
2026-06-16 07:35:41 -0500

    Start in spaceship

--- src/new_game.c
+++ src/new_game.c
@@ -138,7 +138,7 @@ static void WarpToTruck(void)
     if (IS_FRLG)
         SetWarpDestination(MAP_GROUP(MAP_PALLET_TOWN_PLAYERS_HOUSE_2F), MAP_NUM(MAP_PALLET_TOWN_PLAYERS_HOUSE_2F), WARP_ID_NONE, 6, 6);
     else
-        SetWarpDestination(MAP_GROUP(MAP_TEST_ZONE_ONE), MAP_NUM(MAP_TEST_ZONE_ONE), WARP_ID_NONE, -1, -1);
+        SetWarpDestination(MAP_GROUP(MAP_SPACESHIP_DEV), MAP_NUM(MAP_SPACESHIP_DEV), WARP_ID_NONE, -1, -1);
     WarpIntoMap();
 }
 
