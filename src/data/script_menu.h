b46352599f5c1321e069cdf69683780e58bdae26
Joseph N <JosephTnied@gmail.com>
2026-06-21 10:53:26 -0500

    Console change planet works great

--- src/data/script_menu.h
+++ src/data/script_menu.h
@@ -1122,6 +1122,13 @@ static const struct MenuAction sMultichoiceList_HOF_Eggs_Victories_Quit[] = {
     {gText_ShopQuit}
 };
 
+static const struct MenuAction sMultichoiceList_Planet_Select[] = {
+    {COMPOUND_STRING("Earth")},
+    {COMPOUND_STRING("Mars")},
+    {COMPOUND_STRING("Venus")},
+    {gText_Exit}
+};
+
 static const struct MenuAction MultichoiceList_Exit[] =
 {
     {gText_Exit},
@@ -1294,6 +1301,7 @@ static const struct MultichoiceListStruct sMultichoiceLists[] =
     [MULTI_HOF_VICTORIES_QUIT]                         = MULTICHOICE(sMultichoiceList_HOF_Victories_Quit),
     [MULTI_EGGS_VICTORIES_QUIT]                        = MULTICHOICE(sMultichoiceList_Eggs_Victories_Quit),
     [MULTI_HOF_EGGS_VICTORIES_QUIT]                    = MULTICHOICE(sMultichoiceList_HOF_Eggs_Victories_Quit),
+    [MULTI_PLANET_SELECT]                              = MULTICHOICE(sMultichoiceList_Planet_Select),
 };
 
 const u8 *const gStdStrings[] =
